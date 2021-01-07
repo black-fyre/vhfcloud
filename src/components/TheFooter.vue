9cfeebc99d5babf10f082d97264b543542799756
Dahunsi Fehintoluwa <dahunsi.fehinti@gmail.com>
2021-01-07 12:11:17 +0100

    Added footer component to default, to test whether bootstrap works (next commit)

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="alert alert-success alert-dismissible fade show" role="alert">
+    With Bootstrap!
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+name: "TheFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheFooter></TheFooter>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import TheFooter from "@/components/TheFooter";
 
 export default {
   name: 'Home',
   components: {
+    TheFooter,
     HelloWorld
   }
 }
